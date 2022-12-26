7eb4d644a231098960262438bf04e34e3006f5b1
JudithHopps <113181934+JudithHopps@users.noreply.github.com>
2022-12-26 22:18:46 +0900

    유수빈 - 프로젝트 배포

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = defineConfig({});
+
+module.exports = {
+  publicPath: "/raoncorp",
+  outputDir: "docs",
   transpileDependencies: true,
-});
+};
